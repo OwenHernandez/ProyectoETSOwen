7b36e3da44f064b0757d53687e3af3873345c53b
Owen-Daniel-Hernández-Hanrahan <owentoto423@gamil.com>
2023-06-10 23:05:17 +0100

    acabado el javadoc de la clase CCuenta

--- ProyectoETSOwen/src/main/java/cuentas/CCuenta.java
+++ ProyectoETSOwen/src/main/java/cuentas/CCuenta.java
@@ -3,15 +3,21 @@ package cuentas;
 public class CCuenta {
 
 
+    //Nombre de la cuenta
     private String nombre;
+    //Código de la cuenta
     private String cuenta;
+    //Saldo de la cuenta
     private double saldo;
+    //Tipo de interés
     private double tipoInterés;
 
+    //Constructor por defecto
     public CCuenta()
     {
     }
 
+    //Contructor para todo lo demás
     public CCuenta(String nom, String cue, double sal, double tipo)
     {
         nombre =nom;
@@ -19,11 +25,20 @@ public class CCuenta {
         saldo=sal;
     }
 
+    /**
+     * Te muestra la cantidad de saldo en la cuenta
+     * @return Cantidad (en double) del saldo que dispone la cuenta
+     */
     public double estado()
     {
         return saldo;
     }
 
+    /**
+     * Añade la cantidad dada en el saldo de la cuenta y lanza una excepción si dicha cantidad es negativa
+     * @param cantidad a ingresar
+     * @throws Exception 
+     */
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
@@ -31,6 +46,12 @@ public class CCuenta {
         saldo = saldo + cantidad;
     }
 
+    /**
+     * Resta la cantidad dad en el saldo de la cuenta y lanza una excepción si dicha cantidad es negativa o cero
+     * o, si el saldo es menor que la cantidad a retirar
+     * @param cantidad a retirar
+     * @throws Exception 
+     */
     public void retirar(double cantidad) throws Exception
     {
         if (cantidad <= 0)
@@ -40,14 +61,23 @@ public class CCuenta {
         saldo = saldo - cantidad;
     }
     
-    public void operativa_cuenta(String accion, float cantidad) throws Exception{
-        if (accion.toLowerCase().equals("retirar")){
+    /**
+     * unifica los dos métodos ingresar y retirar y lanza la exception de dichos métodos
+     * @param acción, la acción que se desea hacer
+     * @param cantidad de dinero que se pretende retirar o ingresar (depende de la acción)
+     * @throws Exception 
+     */
+    public void operativa_cuenta(String acción, float cantidad) throws Exception{
+        if (acción.toLowerCase().equals("retirar")){
             this.retirar(cantidad);
-        } else if (accion.toLowerCase().equals("ingresar")) {
+        } else if (acción.toLowerCase().equals("ingresar")) {
             this.ingresar(cantidad);
         } 
     }
 
+    /*
+    Apartir de aqui son getter y setter
+    */
     public String getNombre() {
         return nombre;
     }
