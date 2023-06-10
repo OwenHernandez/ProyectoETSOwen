b890f72ff8dfd8b58282311ec5838167a0402673
Owen-Daniel-Hernández-Hanrahan <owentoto423@gamil.com>
2023-06-10 22:51:08 +0100

    encapsulados los atributos de la clase CCuenta

--- ProyectoETSOwen/src/main/java/cuentas/CCuenta.java
+++ ProyectoETSOwen/src/main/java/cuentas/CCuenta.java
@@ -45,6 +45,38 @@ public class CCuenta {
             this.retirar(cantidad);
         } else if (accion.toLowerCase().equals("ingresar")) {
             this.ingresar(cantidad);
-        }
+        } 
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCuenta() {
+        return cuenta;
+    }
+
+    public void setCuenta(String cuenta) {
+        this.cuenta = cuenta;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public double getTipoInterés() {
+        return tipoInterés;
+    }
+
+    public void setTipoInterés(double tipoInterés) {
+        this.tipoInterés = tipoInterés;
     }
 }
